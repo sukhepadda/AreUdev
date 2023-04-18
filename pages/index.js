ff2679d5148625f2410c6871cde566f16f88b3ab
sukhepadda <paddasukhwantsingh@gmail.com>
2023-04-18 21:26:14 +0530

    added boostrap

--- pages/index.js
+++ pages/index.js
@@ -50,33 +50,49 @@ export default function Home() {
   }
 
   return (
-    <div className="grid justify-center items-center text-center text-white">
-      <h1 className="text-4xl font-semibold my-2">Check how long it takes to become developer</h1>
-      <div>
-        <h2 className="text-3xl my-2" id="idealnumberofhourstitle">Ideal Recomendations</h2>
-        <p className="my-2 text-lg" id="idealnumberofhoursstringptag">4 hours per week of tutoring and 12 hours per week of practice</p>
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="description" content="A webpage to check in how much time you will be a developer" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="author" content="Jay" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Are U Dev?</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+      </Head>
+      <div className="container text-center">
+        <h1 className="h2 text-primary my-3 m-2">Check how long it takes to become developer</h1>
+        <div>
+          <h2 className="h5 m-2 text-info" id="idealnumberofhourstitle">Ideal Recomendations</h2>
+          <p className="lead text-white m-2" id="idealnumberofhoursstringptag">4 hours per week of tutoring and 12 hours per week of practice</p>
+        </div>
+        <div className=''>
+          <input className="m-2 form-control" type="number" min="0" id="inputNumbereveryweektutor" placeholder="everyweektutor hours" onChange={e => setTutorHour(e.target.value)} />
+          <input className="m-2 form-control" type="number" min="0" id="inputNumbereveryweekpractice" placeholder="everyweekpractice hours" onChange={e => setPracticeHour(e.target.value)} />
+        </div>
+        <div className="">
+          <button type="button" className="btn btn-primary m-2" onClick={loadreturnestimate}>
+            Load Estimate
+          </button>
+
+          <button type="button" onClick={resetFunc} className="btn btn-danger m-2">
+            Reset Values
+          </button>
+
+          <button type="button" onClick={apiTestFunc} className="btn btn-secondary m-2">
+            Api Test
+          </button>
+
+        </div>
+
+        {/* Output Window */}
+        <h1 className="h5 text-success m-3" id="outputSentence"></h1>
       </div>
-      <div>
-        <input className=" m-2 px-2 py-1 rounded-sm bg-gray-500 text-white placeholder-gray-400 border-2 border-gray-600  bg-transparent focus:outline-none " type="number" min="0" id="inputNumbereveryweektutor" placeholder="everyweektutor hours" onChange={e => setTutorHour(e.target.value)} />
-        <input className=" m-2 px-2 py-1 rounded-sm bg-gray-500 text-white placeholder-gray-400 border-2 border-gray-600  bg-transparent focus:outline-none " type="number" min="0" id="inputNumbereveryweekpractice" placeholder="everyweekpractice hours" onChange={e => setPracticeHour(e.target.value)} />
-      </div>
-      <div className=" flex justify-center items-center ">
-        <button type="button" className="text-white bg-transparent border-2 w-40 border-gray-600 rounded-lg p-1 m-2 hover:bg-gray-700 " onClick={loadreturnestimate}>
-          load return estimate
-        </button>
-
-        <button type="button" onClick={resetFunc} className="text-white bg-transparent border-2 w-40 border-gray-600 rounded-lg p-1 m-2 hover:bg-gray-700 ">
-          reset input
-        </button>
-
-        <button type="button" onClick={apiTestFunc} className="text-white bg-transparent border-2 w-40 border-gray-600 rounded-lg p-1 m-2 hover:bg-gray-700 ">
-          Api Test
-        </button>
-
-      </div>
-
-      {/* Output Window */}
-      <h1 className="block" id="outputSentence"></h1>
-    </div>
+    </>
   )
 }
